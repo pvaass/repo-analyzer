9c4f4bc2b1cdf88cc17ded4683004184a2132e39
pvaass <pvaassens@expandonline.nl>
2017-05-26 16:30:39 +0200

    Add docs for repository package

---
+++ pkg/repository/doc.go
@@ -0,0 +1,3 @@
+// Package repository provides a way to abstract different git hosting services
+// into one common interface and provides functions to interact with these services.
+package repository
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -11,12 +11,16 @@ import (
 	"github.com/pvaass/repo-analyzer/pkg/repository/platforms"
 )
 
+// Repository is the only exported struct in the repository package.
+// It provides a way to interact with different git hosting platforms.
 type Repository struct {
 	URI      string
 	Files    map[string][]platforms.File
 	Platform platforms.Platform
 }
 
+// List returns a list of files from a path in a repository
+// and stores them in Repository for later use, avoiding multiple api requests.
 func (r *Repository) List(path string) []platforms.File {
 	elems, ok := r.Files[path]
 	if !ok {
@@ -26,6 +30,8 @@ func (r *Repository) List(path string) []platforms.File {
 	return r.Files[path]
 }
 
+// File returns the contents of a file from the repository.
+// When no such file exists, it returns error
 func (r *Repository) File(name string) ([]byte, error) {
 	path := filepath.Dir(name)
 	elems, ok := r.Files[path]
@@ -47,6 +53,17 @@ func (r *Repository) File(name string) ([]byte, error) {
 	return []byte{}, errors.New("Could not find file " + name + " in remote repository")
 }
 
+// New configures and returns a new Repository
+func New(platform platforms.Platform, uri string) Repository {
+	r := new(Repository)
+	platform.SetURI(uri)
+	r.URI = uri
+	r.Files = make(map[string][]platforms.File)
+	r.Platform = platform
+
+	return *r
+}
+
 func download(url string) []byte {
 	log.Println("Downloading " + url)
 	req, err := http.NewRequest("GET", url, nil)
@@ -65,14 +82,4 @@ func download(url string) []byte {
 		log.Panic("Body read error", err)
 	}
 	return body
-}
-
-func New(platform platforms.Platform, uri string) Repository {
-	r := new(Repository)
-	platform.SetURI(uri)
-	r.URI = uri
-	r.Files = make(map[string][]platforms.File)
-	r.Platform = platform
-
-	return *r
 }
